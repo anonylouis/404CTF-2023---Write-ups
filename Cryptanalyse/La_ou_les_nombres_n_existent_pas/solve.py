b94b4151bdc25e00a19a098f4910b76e9b9559af
anonylouis <louis.calvie@gmail.com>
2023-06-08 03:09:07 +0200

    clean script + image  CryptoExtreme

--- Cryptanalyse/La_ou_les_nombres_n_existent_pas/solve.py
+++ Cryptanalyse/La_ou_les_nombres_n_existent_pas/solve.py
@@ -6,9 +6,13 @@ from Crypto.Util.Padding import unpad
 from hashlib import sha256
 from solve_quadratic_congruence import solve_quadratic_congruence
 
+def decrypt_flag(cipher, iv, sk):
+    key = sha256(sk).digest()[:16]
+    aes = AES.new(key , AES.MODE_CBC, iv=iv)
+    flag = unpad(aes.decrypt(cipher), 16)
+    print(flag)
 
-q = P521.q
-
+# parsing
 f = open("data.txt", "r")
 
 m =[]
@@ -22,122 +26,60 @@ for i in range(4) :
     r.append(int(l[0]))
     s.append(int(l[1]))
 
-iv = f.readline()[:-1]
-cipher = f.readline()
+iv = bytes.fromhex(f.readline()[:-1])
+cipher = bytes.fromhex(f.readline())
 
-decrypt_d = m[0]
+q = P521.q
+d = m[0]
 
-print("")
+# Equation 3
+C1 = (r[1] * pow(s[1], -1, q) * s[0] * pow(r[0], -1, q) - r[2] * pow(s[2], -1, q) * pow(r[1], -1, q) * s[1] + d) % q
+C2 = (pow(r[1], -1, q) * s[1] - s[0] * pow(r[0], -1, q)) % q
+C3 = (m[1] * pow(r[1], -1, q) - m[0] * pow(r[0], -1, q)) % q
+C4 = (m[2] * pow(s[2], -1, q) * pow(r[1], -1, q) * s[1] - d * pow(r[1], -1, q) * m[1] - m[1] * pow(s[1], -1, q) * s[0] * pow(r[0], -1, q)) % q
 
-C1 = m[2] * pow(s[2], -1, q) * pow(r[1], -1, q) * s[1] - decrypt_d * pow(r[1], -1, q) * m[1] - m[1] * pow(s[1], -1, q) * s[0] * pow(r[0], -1, q)
-C2 = pow(r[1], -1, q) * s[1] - s[0] * pow(r[0], -1, q)
-C3 = m[1] * pow(r[1], -1, q) - m[0] * pow(r[0], -1, q)
-C4 = r[1] * pow(s[1], -1, q) * s[0] * pow(r[0], -1, q) - r[2] * pow(s[2], -1, q) * pow(r[1], -1, q) * s[1] + decrypt_d
-C1 %=q
-C2 %=q
-C3 %=q
-C4 %=q
-
-# # Equation 1
-# print((C.sk * C4 + _B * C2 + _A * C3)  % q)
-# print(C1  % q)
+# (C.sk * C1 + _B * C2 + _A * C3)  % q = C4 % q
 
 
-print("")
+#Equation 4
+C5 = (r[1] * pow(s[1], -1, q) * s[0] * pow(r[0], -1, q) -  s[2] * pow(r[2], -1, q) * pow(s[3], -1, q) * r[3] + 2 * d ) % q
+C6 = (s[2] * pow(r[2], -1, q) - s[0] * pow(r[0], -1, q) ) % q
+C7 = (pow(r[2], -1, q) * m[2] - m[0] * pow(r[0], -1, q) ) % q
+C8 = (s[2] * pow(r[2], -1, q) * pow(s[3], -1, q) * m[3] - m[1] * pow(s[1], -1, q) * s[0] * pow(r[0], -1, q) - 2 * d * pow(r[2], -1, q) * m[2] ) % q
 
-C5 = r[1] * pow(s[1], -1, q) * s[0] * pow(r[0], -1, q) -  s[2] * pow(r[2], -1, q) * pow(s[3], -1, q) * r[3] + 2 * decrypt_d
-C6 = s[2] * pow(r[2], -1, q) - s[0] * pow(r[0], -1, q)
-C7 = pow(r[2], -1, q) * m[2] - m[0] * pow(r[0], -1, q)
-C8 = s[2] * pow(r[2], -1, q) * pow(s[3], -1, q) * m[3] - m[1] * pow(s[1], -1, q) * s[0] * pow(r[0], -1, q) - 2 * decrypt_d * pow(r[2], -1, q) * m[2]
+# (C.sk * C5 + _B * C6 + _A * C7) % q = C8 % q
 
+# a en fonction de sk 
+tmp = (C3 - C2 * C7  * pow(C6, -1, q))
+F1 = (C4 - C2 * C8 * pow(C6, -1, q)) * pow(tmp, -1, q)
+F2 = (C1 - C2 * C5 * pow(C6, -1, q)) * pow(tmp, -1, q)
+# a % q = (F1 - C.sk * F2)  % q)
 
-C5 %=q
-C6 %=q
-C7 %=q
-C8 %=q
-#Equation 2
+# b en fonction de sk 
+tmp = (C2 - C3 * C6 * pow(C7, -1, q))
+E1 = (C4 - C3 * C8 * pow(C7, -1, q)) * pow(tmp, -1, q)
+E2 = (C1 - C3 * C5 * pow(C7, -1, q)) * pow(tmp, -1, q)
+# b % q = (E1 - E2 * C.sk)  % q)
 
-# print((C.sk * C5 + _B * C6 + _A * C7) % q)
-# print(C8  % q)
-
-
-print("")
-
-C9  = m[1] * s[0]
-C10 = -m[1] * r[0]
-C11 = m[2]
-
-C9 %=q
-C10 %=q
-C11 %=q
-
-# eqatiom 3
-
-# print((s[0] *  _K ) % q)
-# print((m[0] + C.sk * r[0]) % q)
-
-# print((C9 * _K + C10 * C.sk) % q)
-# print(C11 % q)
+# k en fonction de sk 
+G1 = m[0] * pow(s[0], -1, q)
+G2 = r[0] * pow(s[0], -1, q)
+# k % q = (G1 + C.sk * G2) % q
 
 
-#ON COMBINE EQ1 ET EQ2 :
-print("")
+# equation 2
 
-# C4 * sk + C2 * B + C3 * A = C1
-# C5 * SK + C6 * B + C7 * A = C8
+H1 = (F2 * G2) % q
+H2 = (F2 * G1 - F1 * G2 + E2 + r[1] * pow(s[1], -1, q)) % q
+H3 = (m[1] * pow(s[1], -1, q) - F1 * G1 - E1) % q
 
+# H1 * C.sk * C.sk + H2 * C.sk + H3) % q = 0
 
-# print((C.sk * C5 * pow(C7, -1, q) + _B  * C6 * pow(C7, -1, q) + _A ) % q)
-# print((C8 * pow(C7, -1, q))  % q)
-
-print("B en fonction de sk")
-
-E2 = (C2 - C3 * C6 * pow(C7, -1, q)) 
-E1 = (C4 - C3 * C5 * pow(C7, -1, q)) * pow(E2, -1, q)
-E3 = (C1 - C3 * C8 * pow(C7, -1, q)) *  pow(E2, -1, q)
-# print( _B  % q)
-# print((E3 - E1 * C.sk)  % q)
-
-print("\nA en fonction de sk")
-
-F2 = (C3 - C2 * C7  * pow(C6, -1, q))
-F3 = (C1 - C2 * C8 * pow(C6, -1, q)) * pow(F2, -1, q)
-F1 = (C4 - C2 * C5 * pow(C6, -1, q)) * pow(F2, -1, q)
-# print(_A  % q)
-# print((F3 - C.sk * F1)  % q)
-
-print("\nK en fonction de sk")
-
-G1 = r[0] * pow(s[0], -1, q)
-G2 = m[0] * pow(s[0], -1, q)
-# print( _K % q)
-# print((G2 + C.sk * G1) % q)
-
-
-print("\n On insere tout dans eq2 :")
-
-#print(((F3 - C.sk * F1) * (G2 + C.sk * G1) + _B) % q)
-
-H1 = F3 * G1 - F1 * G2 - E1 - r[1] * pow(s[1], -1, q)
-H2 = - (F1 * G1)
-H3 = m[1] * pow(s[1], -1, q) - (F3 * G2 +  E3)
-
-H1 *=  pow(H2, -1, q)
-H3 *=  pow(H2, -1, q)
-
-H3 *= -1
-H1 %= q
-H2 %= q
-H3 %= q
-
-# print((C.sk * C.sk + H1 * C.sk + H3) % q)
-
-L = solve_quadratic_congruence(1, H1, H3, q)
+L = solve_quadratic_congruence(H1, H2, H3, q)
 
 for i in L :
-    if i == L[0] :
-        decrypt_iv = bytes.fromhex(iv)
-        decrypt_key = sha256(str(i).encode()).digest()[:16]
-        decrypt_aes = AES.new(decrypt_key , AES.MODE_CBC, iv=decrypt_iv)
-        print(unpad(decrypt_aes.decrypt(bytes.fromhex(cipher)),16))
-
+    try :
+        sk = str(i).encode()
+        decrypt_flag(cipher, iv, sk)
+    except :
+        continue
